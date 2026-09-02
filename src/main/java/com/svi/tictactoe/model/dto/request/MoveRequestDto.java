fcdc7ebfb48ff096d063faffbc27cf0cc6e8eadc
Xeth Soto <xsoto@svi.com.ph>
2026-09-02 11:05:44 +0800

    feat: add dateSave as field in request. Add getters for the rest of the fields

--- src/main/java/com/svi/tictactoe/model/dto/request/MoveRequestDto.java
+++ src/main/java/com/svi/tictactoe/model/dto/request/MoveRequestDto.java
@@ -1,18 +1,22 @@
 package com.svi.tictactoe.model.dto.request;
 
+import javax.json.bind.annotation.JsonbProperty;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class MoveRequestDto {
 
   @NotBlank(message = "Game ID cannot be blank")
+  @JsonbProperty("gameid")
   private String gameId;
 
   @NotBlank(message = "Player ID cannot be blank")
-  private String playerId;
+  @JsonbProperty("playername")
+  private String playerName;
 
   @NotBlank(message = "Symbol cannot be blank")
   @Pattern(regexp = "[XO]", message = "Symbol must be either 'X' or 'O'")
@@ -22,12 +26,15 @@ public class MoveRequestDto {
   @Max(value = 8, message = "Location must be between 0 and 8")
   private int location;
 
+  @JsonbProperty("datetime")
+  private LocalDateTime dateSave;
+
   public String getGameId() {
     return gameId;
   }
 
-  public String getPlayerId() {
-    return playerId;
+  public String getPlayerName() {
+    return playerName;
   }
 
   public String getSymbol() {
@@ -36,5 +43,9 @@ public class MoveRequestDto {
 
   public int getLocation() {
     return location;
+  }
+
+  public LocalDateTime getDateSave() {
+    return dateSave;
   }
 }
