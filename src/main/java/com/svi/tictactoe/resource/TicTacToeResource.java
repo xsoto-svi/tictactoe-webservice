3bf707b40eac1d33642b1a3aa57b8d2e6ee12ba1
Xeth Soto <xsoto@svi.com.ph>
2026-09-02 11:36:02 +0800

    feat: add getGamesByPlayerId() and getGameDetailsByGameId()

--- src/main/java/com/svi/tictactoe/resource/TicTacToeResource.java
+++ src/main/java/com/svi/tictactoe/resource/TicTacToeResource.java
@@ -1,21 +1,72 @@
 package com.svi.tictactoe.resource;
 
 import com.svi.tictactoe.model.dto.request.MoveRequestDto;
+import com.svi.tictactoe.model.dto.response.GameDetailsResponse;
+import com.svi.tictactoe.model.dto.response.GameMoveResponseDto;
+import com.svi.tictactoe.model.dto.response.ListGamesResponse;
+import com.svi.tictactoe.service.TicTacToeService;
 
+import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Path("")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class TicTacToeResource {
 
+  @Inject
+  private TicTacToeService ticTacToeService;
+
   @POST
   @Path("/save")
   public Response saveMove(@Valid MoveRequestDto moveDto) {
     return Response.ok().build();
+  }
+
+  @GET
+  @Path("/list-games/{playerId}")
+  public Response getGamesByPlayerId(@PathParam("playerId") String playerName) {
+    List<UUID> gameUUIDList = ticTacToeService.getGamesByPlayerName(playerName);
+
+    List<ListGamesResponse.GameItemDto> gameItemList = gameUUIDList.stream()
+            .map(ListGamesResponse.GameItemDto::new)
+            .collect(Collectors.toList());
+
+    if (gameItemList.isEmpty()) {
+      return Response.ok()
+              .type(MediaType.APPLICATION_JSON)
+              .entity(new ListGamesResponse(gameItemList, "No records found."))
+              .build();
+    }
+
+    return Response.ok()
+            .type(MediaType.APPLICATION_JSON)
+            .entity(new ListGamesResponse(gameItemList, "Records found."))
+            .build();
+  }
+
+  @GET
+  @Path("/game/{gameId}")
+  public Response getGameDetailsByGameId(@PathParam("gameId") UUID gameId) {
+    List<GameMoveResponseDto> gameDetailsList = ticTacToeService.getGameDetailsByGameId(gameId);
+
+    if (gameDetailsList.isEmpty()) {
+      return Response.ok()
+              .type(MediaType.APPLICATION_JSON)
+              .entity(new GameDetailsResponse(gameDetailsList, "No records found"))
+              .build();
+    }
+
+    return Response.ok()
+            .type(MediaType.APPLICATION_JSON)
+            .entity(new GameDetailsResponse(gameDetailsList, "Records found."))
+            .build();
   }
 
 
--- src/main/java/com/svi/tictactoe/service/TicTacToeService.java
+++ src/main/java/com/svi/tictactoe/service/TicTacToeService.java
@@ -2,24 +2,29 @@ package com.svi.tictactoe.service;
 
 import com.svi.tictactoe.mapper.GameMoveMapper;
 import com.svi.tictactoe.model.dto.request.MoveRequestDto;
+import com.svi.tictactoe.model.dto.response.GameMoveResponseDto;
 import com.svi.tictactoe.model.entity.GameMove;
 import com.svi.tictactoe.repository.FileGameRepository;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.UUID;
 
 public class TicTacToeService {
 
   @Inject
   private FileGameRepository fileGameRepository;
 
-  public String saveMoveOnTxtFile(MoveRequestDto moveRequestDto) {
+  public void saveMove(MoveRequestDto moveRequestDto) {
     GameMove move = GameMoveMapper.toEntity(moveRequestDto);
     fileGameRepository.saveMoveOnTxtFile(move);
-
-    return "Record saved.";
   }
 
-//  public ListGamesResponseDto listGames() {
-//    return new ListGamesResponseDto();
-//  }
+  public List<UUID> getGamesByPlayerName(String name) {
+    return fileGameRepository.getGamesByPlayerName(name);
+  }
+
+  public List<GameMoveResponseDto> getGameDetailsByGameId(UUID id) {
+    return fileGameRepository.getGameDetailsByGameId(id);
+  }
 }
