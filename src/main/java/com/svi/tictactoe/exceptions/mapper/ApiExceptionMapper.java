d1850cb471c807f286800d4a82699a7a4ddfec36
Xeth Soto <xsoto@svi.com.ph>
2026-09-03 09:54:00 +0800

    chore: add @Provider annotation

--- src/main/java/com/svi/tictactoe/exceptions/mapper/ApiExceptionMapper.java
+++ src/main/java/com/svi/tictactoe/exceptions/mapper/ApiExceptionMapper.java
@@ -1,4 +1,21 @@
 package com.svi.tictactoe.exceptions.mapper;
 
-public class ApiExceptionMapper {
+import com.svi.tictactoe.exceptions.ApiException;
+import com.svi.tictactoe.model.dto.response.ApiResponse;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ApiExceptionMapper implements ExceptionMapper<ApiException> {
+
+  @Override
+  public Response toResponse(ApiException exception) {
+    return Response.status(exception.getStatus())
+            .type(MediaType.APPLICATION_JSON)
+            .entity(new ApiResponse(exception.getMessage()))
+            .build();
+  }
 }
