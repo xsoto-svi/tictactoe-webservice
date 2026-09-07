3adc8dd465a532963d5cbd8e95f6c7598ea9f3cd
Xeth Soto <xsoto@svi.com.ph>
2026-09-07 18:14:07 +0800

    chore: remove unused imports

--- src/main/java/com/svi/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/svi/tictactoe/TicTacToeApplication.java
@@ -1,24 +1,8 @@
 package com.svi.tictactoe;
 
-import com.svi.tictactoe.model.dto.response.ApiResponse;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("api")
 public class TicTacToeApplication extends Application {
-
-  @GET
-  @Path("health")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response checkHealth() {
-    return Response.ok()
-            .entity(new ApiResponse("Server is running."))
-            .build();
-  }
 }
